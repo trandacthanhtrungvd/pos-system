b2a998a5efe8df509694bbd20f2447869a0d5d3e
Thanh Trung <trandacthanhtrungvd@gmail.com>
2023-11-21 23:39:58 +0700

    Minor changes in Status components

--- src/components/Status/RoomStatus.jsx
+++ src/components/Status/RoomStatus.jsx
@@ -8,19 +8,13 @@ const RoomStatus = ({ status }) => {
     na: "Not Available",
   };
 
-  const defaultFormat =
-    "inline-flex h-6 w-28 items-center justify-center gap-2.5 rounded-3xl bg-opacity-25 px-3 py-1 font-['Barlow'] text-sm font-medium leading-tight ";
-
   const statusFormat = {
     a: "bg-accents-green text-accents-green",
     na: "bg-accents-red text-accents-red",
   };
 
-  const format = defaultFormat.concat(statusFormat[status]);
-  console.log(format);
-
   return (
-    <div className={defaultFormat.concat(statusFormat[status])}>
+    <div className={`inline-flex h-6 w-28 items-center justify-center gap-2.5 rounded-3xl bg-opacity-25 px-3 py-1 font-['Barlow'] text-sm font-medium leading-tight ${statusFormat[status]}`}>
       {statusText[status]}
     </div>
   );
